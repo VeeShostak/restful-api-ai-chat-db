11f54a545cc6dab657568b987bc5ff21a4ce0f4d
VeeShostak <VeeShostak@users.noreply.github.com>
2017-12-07 21:45:58 -0500

    implement /auth endpoint for JWT

---
+++ security.py
@@ -0,0 +1,15 @@
+from werkzeug.security import safe_str_cmp
+from models.user import UserModel
+
+def authenticate(id, email):
+	# check if uid exists in db (if yes, then user is registered with firebase)
+
+    user = UserModel.find_by_id(id)
+    if user:
+    	return user
+    # if user and safe_str_cmp(user.password, password):
+    #     return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return UserModel.find_by_id(user_id)
